f1bc370c0bbbfc6b8708a7788db115480daec684
srinath991 <srinathvsrinathv863@gmail.com>
2024-12-20 13:08:04 +0530

    complition of data ingestion component

--- main.py
+++ main.py
@@ -1,2 +1,15 @@
 from kidneyDiseaseNet import logger
-logger.info("KidneyDiseaseNet application started.")
+from kidneyDiseaseNet.pipeline.stage_01_data_ingestion import DataIngestionPipeline
+
+
+
+
+
+STAGE_NAME = "Data Ingestion stage"
+try:
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
+   data_ingestion = DataIngestionPipeline()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
